b13eaf3185f038a5114697cccc8c8f99ebdd53cc
ollie.j <ollie.j@klaytn.foundation>
2024-08-06 12:06:28 +0900

    contracts: MultiCallContract compiled with 0.8.19

--- kaiax/staking/getter_test.go
+++ kaiax/staking/getter_test.go
@@ -16,19 +16,6 @@ import (
 
 func TestGetStakingInfo_PostKaia(t *testing.T) {
 	log.EnableLogForTest(log.LvlCrit, log.LvlWarn)
-
-	// Create a simulated backend
-	config := params.TestChainConfig.Copy()
-	config.IstanbulCompatibleBlock = common.Big0
-	config.LondonCompatibleBlock = common.Big0
-	config.EthTxTypeCompatibleBlock = common.Big0
-	config.MagmaCompatibleBlock = common.Big0
-	config.KoreCompatibleBlock = common.Big0
-	config.ShanghaiCompatibleBlock = common.Big0
-	config.CancunCompatibleBlock = common.Big0
-	config.KaiaCompatibleBlock = common.Big0
-	config.SetDefaults()
-
 	var (
 		db    = database.NewMemoryDBManager()
 		alloc = blockchain.GenesisAlloc{
@@ -36,17 +23,42 @@ func TestGetStakingInfo_PostKaia(t *testing.T) {
 				Code:    system.AddressBookMockTwoCNCode,
 				Balance: big.NewInt(0),
 			},
+			common.HexToAddress("0x0000000000000000000000000000000000000F01"): { // staking1
+				Balance: new(big.Int).Mul(big.NewInt(5_000_000), big.NewInt(params.KAIA)),
+			},
+			common.HexToAddress("0x0000000000000000000000000000000000000f04"): { // staking2
+				Balance: new(big.Int).Mul(big.NewInt(10_000_000), big.NewInt(params.KAIA)),
+			},
+		}
+		config = &params.ChainConfig{
+			ChainID: common.Big1,
+			Governance: &params.GovernanceConfig{
+				Reward: &params.RewardConfig{
+					UseGiniCoeff:          false,
+					StakingUpdateInterval: 86400,
+				},
+			},
 		}
 		backend = backends.NewSimulatedBackendWithDatabase(db, alloc, config)
-	)
 
-	// Force using the MultiCallMock
-	originCode := system.MultiCallCode
-	system.MultiCallCode = system.MultiCallMockCode
-	defer func() {
-		system.MultiCallCode = originCode
-		backend.Close()
-	}()
+		// Addresses taken from AddressBookMock.sol:AddressBookMockTwoCN
+		// Staking amounts taken from MultiCallContractMock.sol
+		expected = &StakingInfo{
+			SourceBlockNum: 0,
+			NodeIds: []common.Address{
+				common.HexToAddress("0x0000000000000000000000000000000000000F00"),
+				common.HexToAddress("0x0000000000000000000000000000000000000F03")},
+			StakingContracts: []common.Address{
+				common.HexToAddress("0x0000000000000000000000000000000000000F01"),
+				common.HexToAddress("0x0000000000000000000000000000000000000f04")},
+			RewardAddrs: []common.Address{
+				common.HexToAddress("0x0000000000000000000000000000000000000f02"),
+				common.HexToAddress("0x0000000000000000000000000000000000000f05")},
+			KIFAddr:        common.HexToAddress("0x0000000000000000000000000000000000000F06"),
+			KEFAddr:        common.HexToAddress("0x0000000000000000000000000000000000000f07"),
+			StakingAmounts: []uint64{5_000_000, 10_000_000},
+		}
+	)
 
 	// Test GetStakingInfo()
 	mStaking := NewStakingModule()
@@ -55,24 +67,6 @@ func TestGetStakingInfo_PostKaia(t *testing.T) {
 		ChainConfig: config,
 		Chain:       backend.BlockChain(),
 	})
-
-	// Addresses taken from AddressBookMock.sol:AddressBookMockTwoCN
-	// Staking amounts taken from MultiCallContractMock.sol
-	expected := &StakingInfo{
-		SourceBlockNum: 0,
-		NodeIds: []common.Address{
-			common.HexToAddress("0x0000000000000000000000000000000000000F00"),
-			common.HexToAddress("0x0000000000000000000000000000000000000F03")},
-		StakingContracts: []common.Address{
-			common.HexToAddress("0x0000000000000000000000000000000000000F01"),
-			common.HexToAddress("0x0000000000000000000000000000000000000f04")},
-		RewardAddrs: []common.Address{
-			common.HexToAddress("0x0000000000000000000000000000000000000f02"),
-			common.HexToAddress("0x0000000000000000000000000000000000000f05")},
-		KIFAddr:        common.HexToAddress("0x0000000000000000000000000000000000000F06"),
-		KEFAddr:        common.HexToAddress("0x0000000000000000000000000000000000000f07"),
-		StakingAmounts: []uint64{5_000_000, 20_000_000},
-	}
 	si, err := mStaking.GetStakingInfo(0)
 	assert.Nil(t, err)
 	assert.Equal(t, expected, si)
